d22fe0c6ab54b14906aa77686f7d574407c41642
AhmedElghrbawy <ahmedelghrbawy420@gmail.com>
2020-10-20 17:19:52 +0200

    defined models and copied login/register/logout

--- friendship/models.py
+++ friendship/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField()
+    avatar = models.ImageField(upload_to="friendship/uploads/")
+    passions = models.ManyToManyField('Passion', related_name="passions")
+    bio = models.CharField(max_length=1000)
+    friends = models.ManyToManyField('self')
+
+
+class Passion(models.Model):
+    name = models.CharField(max_length=100)
+
+class Request(models.Model):
+    sender = models.ForeignKey(User, related_name="sentRequests", on_delete=models.CASCADE)
+    to = models.ForeignKey(User, related_name="receivedRequests", on_delete=models.CASCADE)
